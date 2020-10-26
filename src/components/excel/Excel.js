1f72519a909e611e42522bd49de6dcde39e1b7ca
Darkhan <darkhanahmetov228@gmail.com>
2020-10-26 23:11:25 +0600

    feat: added 2 methods getRoot and render in Excel class

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,22 @@ export default class Excel {
     this.$el = document.querySelector(selector);
     this.components = options.components || [];
   }
+
+  getRoot() {
+    const $root = document.createElement('div');
+
+    this.components.forEach((Component) => {
+      const component = new Component();
+      $root.insertAdjacentHTML('beforeend', component.toHTML());
+    });
+
+    // $root.classList.add('excel');
+
+    return $root;
+  }
+
+  render() {
+    // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`);
+    this.$el.append(this.getRoot());
+  }
 }
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,3 +1,7 @@
 import ExcelComponent from '@core/ExcelComponent';
 
-export default class Table extends ExcelComponent {}
+export default class Table extends ExcelComponent {
+  toHTML() {
+    return '<h1>Table</h1>';
+  }
+}
