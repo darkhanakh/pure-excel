6da8083386bde01b4ea606f68b112e82be475496
Darkhan <darkhanahmetov228@gmail.com>
2020-10-27 01:51:18 +0600

    feat: finished integrating mini lib to work with DOM, added some methods

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,7 +2,7 @@ import $ from '@core/dom';
 
 export default class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector);
+    this.$el = $(selector);
     this.components = options.components || [];
   }
 
@@ -12,7 +12,7 @@ export default class Excel {
     this.components.forEach((Component) => {
       const $el = $.create('div', Component.className);
       const component = new Component($el);
-      $el.innerHTML = component.toHTML();
+      $el.html(component.toHTML());
       $root.append($el);
     });
 
@@ -20,7 +20,6 @@ export default class Excel {
   }
 
   render() {
-    // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`);
     this.$el.append(this.getRoot());
   }
 }
