ac9ca786ad6e72a554872262e65cacb3d9f5ef86
Darkhan <darkhanahmetov228@gmail.com>
2020-10-27 01:51:18 +0600

    feat: finished integrating mini lib to work with DOM, added some methods

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,9 +1,40 @@
 'use strict';
 
-class Dom {}
+class Dom {
+  constructor(selector) {
+    this.$el = typeof selector === 'string' ? document.querySelector(selector) : selector;
+  }
 
-export default function $() {
-  return new Dom();
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html;
+      return this;
+    } else {
+      return this.$el.outerHTMl.trim();
+    }
+  }
+
+  clear() {
+    this.html('');
+    return this;
+  }
+
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el;
+    }
+
+    if (Element.prototype.append) {
+      this.$el.append(node);
+    } else {
+      this.$el.appendChild(node);
+    }
+    return this;
+  }
+}
+
+export default function $(selector) {
+  return new Dom(selector);
 }
 
 $.create = (tagName, classes = '') => {
@@ -12,5 +43,5 @@ $.create = (tagName, classes = '') => {
     el.classList.add(classes);
   }
 
-  return el;
+  return $(el);
 };
