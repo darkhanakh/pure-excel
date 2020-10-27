276021b1f41eb58c00f9e8c3a06bb6b4d74688b0
Darkhan <darkhanahmetov228@gmail.com>
2020-10-27 16:35:23 +0600

    feat: prepared structure to work with event listeners in DOMListener class

--- src/core/DOMListener.js
+++ src/core/DOMListener.js
@@ -1,10 +1,17 @@
 'use strict';
 
 export default class DOMListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error('No $root provided to DOMListener class');
     }
     this.$root = $root;
+    this.listeners = listeners;
   }
+
+  createDOMListeners() {
+    console.log(this.listeners);
+  }
+
+  removeDOMListeners() {}
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,16 @@
 import DOMListener from '@core/DOMListener';
 
 export default class ExcelComponent extends DOMListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+  }
+
   // Возвращает шаблон компонента
   toHTMl() {
     return '';
+  }
+
+  init() {
+    this.createDOMListeners();
   }
 }
