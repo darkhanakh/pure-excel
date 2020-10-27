26f4bd2232ead89e990a2877bc3e04abac785fb8
Darkhan <darkhanahmetov228@gmail.com>
2020-10-27 16:35:23 +0600

    feat: prepared structure to work with event listeners in DOMListener class

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,10 +3,21 @@ import ExcelComponent from '@core/ExcelComponent';
 export default class Formula extends ExcelComponent {
   static className = 'excel__formula';
 
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input'],
+    });
+  }
+
   toHTML() {
     return `
     <div class="excel__formula-icon">fx</div>
     <div class="excel__formula-input" contenteditable spellcheck="false"></div>
     `;
+  }
+
+  onInput(e) {
+    console.log('Formula: On Input', e);
   }
 }
