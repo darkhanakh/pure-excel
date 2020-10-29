cc1910aa91239443bbd4994ae8e47e471938e2be
Darkhan <darkhanahmetov228@gmail.com>
2020-10-29 14:29:53 +0600

    feat: created resize branch and tested event listeners on columns

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,26 @@ import createTable from './table.template';
 export default class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      listeners: ['click', 'mousedown', 'mousemove'],
+    });
+  }
+
   toHTML() {
     return createTable(50);
+  }
+
+  onClick() {
+    console.log('Click');
+  }
+
+  onMousedown() {
+    console.log('mousedown');
+  }
+
+  onMousemove() {
+    console.log('mousemove');
   }
 }
