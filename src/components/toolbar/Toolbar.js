44783110993b5a20354ce46fd80dd46aeec98623
Darkhan <darkhanahmetov228@gmail.com>
2020-11-11 13:09:35 +0600

    feat: added state for toolbar buttons

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,27 +1,48 @@
-import ExcelComponent from '@core/ExcelComponent';
 import createToolbar from "@/components/toolbar/toolbar.template";
+import ExcelStateComponent from "@core/ExcelStateComponent";
 import $ from "@core/dom";
 
-export default class Toolbar extends ExcelComponent {
-  static className = 'excel__toolbar';
+export default class Toolbar extends ExcelStateComponent {
+  static className = "excel__toolbar";
 
   constructor($root, options) {
     super($root, {
-      name: 'Toolbar',
-      listeners: ['click'],
-      ...options
+      name: "Toolbar",
+      listeners: ["click"],
+      ...options,
     });
   }
 
+  prepare() {
+    const initialState = {
+      textAlign: "left",
+      fontWeight: "normal",
+      textDecoration: "none",
+      fontStyle: "normal",
+    };
+    this.initState(initialState);
+  }
+
+  get template() {
+    return createToolbar(this.state);
+  }
+
   toHTML() {
-    return createToolbar();
+    return this.template;
   }
 
   onClick(e) {
     const $target = $(e.target);
     const $parent = $target.parent('[data-type="button"]');
     if ($parent) {
-      console.log($parent.dataset.value);
+      if ($parent.dataset.value) {
+        const style = JSON.parse($parent.dataset.value);
+        const key = Object.keys(style)[0];
+        this.setState({
+          [key]: style[key],
+        });
+        console.log(this.state);
+      }
     }
   }
 }
--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -11,37 +11,37 @@ const toButton = button => {
 };
 
 
-export default function createToolbar() {
+export default function createToolbar(state) {
   const buttons = [
       {
           icon: 'format_align_left',
-          active: false,
+          active: state['textAlign'] === 'left',
           value: {textAlign: 'left'},
       },
       {
           icon: 'format_align_justify',
-          active: false,
+          active: state['textAlign'] === 'center',
           value: {textAlign: 'center'},
       },
       {
           icon: 'format_align_right',
-          active: false,
+          active: state['textAlign'] === 'right',
           value: {textAlign: 'right'},
       },
       {
           icon: 'format_bold',
-          active: false,
-          value: {fontWeight: 'bold'},
+          active: state['fontWeight'] === 'bold',
+          value: {fontWeight: state['fontWeight'] === 'bold' ? 'normal' : 'bold'},
       },
       {
           icon: 'format_italic',
-          active: false,
-          value: {fontStyle: 'italic'},
+          active: state['fontStyle'] === 'italic',
+          value: {fontStyle: state['fontStyle'] === 'italic' ? 'normal' : 'italic'},
       },
       {
           icon: 'format_underlined',
-          active: false,
-          value: {textDecoration: 'underline'},
+          active: state['textDecoration'] === 'underline',
+          value: {textDecoration: state['textDecoration'] === 'underline' ? 'none' : 'underline'},
       },
   ];
   return buttons.map(toButton).join('');
